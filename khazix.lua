f822f83ec6bdc831c8a6690ca396ec3c753ef450
allwillburn <livefortoday470@gmail.com>
2017-01-01 20:28:00 -0500

    Update khazix.lua

--- khazix.lua
+++ khazix.lua
@@ -8,7 +8,7 @@ else
 end
 
 
-if GetObjectName(GetMyHero()) ~= "Khazix" then return end
+
 
 
 require("DamageLib")
@@ -35,21 +35,6 @@ KhazixMenu.LaneClear:Boolean("Tiamat", "Use Tiamat", true)
 
 
 
-KhazixMenu:SubMenu("KillSteal", "KillSteal")
-KhazixMenu.KillSteal:Boolean("Q", "KS w Q", true)
-KhazixMenu.KillSteal:Boolean("W", "KS w W", true)
-KhazixMenu.KillSteal:Boolean("E", "KS w E", true)
-
-KhazixMenu:SubMenu("AutoIgnite", "AutoIgnite")
-KhazixMenu.AutoIgnite:Boolean("Ignite", "Ignite if killable", true)
-
-KhazixMenu:SubMenu("Drawings", "Drawings")
-KhazixMenu.Drawings:Boolean("DQ", "Draw Q Range", true)
-
-KhazixMenu:SubMenu("SkinChanger", "SkinChanger")
-KhazixMenu.SkinChanger:Boolean("Skin", "UseSkinChanger", true)
-KhazixMenu.SkinChanger:Slider("SelectedSkin", "Select A Skin:", 1, 0, 4, 1, function(SetDCP) HeroSkinChanger(myHero, SetDCP)  end, true)
-
 OnTick(function (myHero)
 	local target = GetCurrentTarget()
         local YGB = GetItemSlot(myHero, 3142)
@@ -65,44 +50,7 @@ OnTick(function (myHero)
        
 
 
-         --AUTO IGNITE
-	for _, enemy in pairs(GetEnemyHeroes()) do
-		
-		if GetCastName(myHero, SUMMONER_1) == 'SummonerDot' then
-			 Ignite = SUMMONER_1
-			if ValidTarget(enemy, 600) then
-				if 20 * GetLevel(myHero) + 50 > GetCurrentHP(enemy) + GetHPRegen(enemy) * 3 then
-					CastTargetSpell(enemy, Ignite)
-				end
-			end
 
-		elseif GetCastName(myHero, SUMMONER_2) == 'SummonerDot' then
-			 Ignite = SUMMONER_2
-			if ValidTarget(enemy, 600) then
-				if 20 * GetLevel(myHero) + 50 > GetCurrentHP(enemy) + GetHPRegen(enemy) * 3 then
-					CastTargetSpell(enemy, Ignite)
-				end
-			end
-		end
-
-	end
-
-        for _, enemy in pairs(GetEnemyHeroes()) do
-                
-                if IsReady(_Q) and ValidTarget(enemy, 325) and KhazixMenu.KillSteal.Q:Value() and GetHP(enemy) < getdmg("Q",enemy) then
-		         if target ~= nil then 
-                                      CastTargetSpell(target, _Q)
-		         end
-                end 
-                
-                if IsReady(_W) and ValidTarget(enemy, 275) and KhazixMenu.KillSteal.W:Value() and GetHP(enemy) < getdmg("W",enemy) then
-		                      CastSkillShot(_W, target)
-
-                if IsReady(_E) and ValidTarget(enemy, 700) and KhazixMenu.KillSteal.E:Value() and GetHP(enemy) < getdmg("E",enemy) then
-		                      CastSkillShot(_E, target)
-  
-                end
-      end
 
       if Mix:Mode() == "LaneClear" then
       	  for _,closeminion in pairs(minionManager.objects) do
@@ -125,34 +73,19 @@ OnTick(function (myHero)
 		if KhazixMenu.LaneClear.RHydra:Value() and ValidTarget(closeminion, 400) then
                         CastTargetSpell(closeminion, RHydra)
       	        end
-          end
-      end
-			end
+          
+         end
+		
        
      end         
 	
 end)
 
-OnDraw(function (myHero)
-        
-         if KhazixMenu.Drawings.DE:Value() then
-		DrawCircle(GetOrigin(myHero), 700, 0, 200, GoS.Black)
-	end
-
-end)
 
 
 
 
 
-local function SkinChanger()
-	if KhazixMenu.SkinChanger.UseSkinChanger:Value() then
-		if SetDCP >= 0  and SetDCP ~= GlobalSkin then
-			HeroSkinChanger(myHero, SetDCP)
-			GlobalSkin = SetDCP
-		end
-        end
-end)
 
 
 print('<font color = "#01DF01"><b>Khazix</b> <font color = "#01DF01">by <font color = "#01DF01"><b>Allwillburn</b> <font color = "#01DF01">Loaded!')
